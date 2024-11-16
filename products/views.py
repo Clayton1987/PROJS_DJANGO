56d8bb27d676b51e4a19a2acb7537cb7151ca964
Clayton1987 <claytonunip@yahoo.com.br>
2024-11-16 17:28:55 -0300

    "Added product_view to urls.py and created product_view function in views.py"

--- products/views.py
+++ products/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from products.models import Product
 
 # Create your views here.
+#def product_view(requests):
+#    return HttpResponse('Hello World!!!!!!!!! JOWWWWHHH')
+
+def product_view(request):
+    print(dir(request))
+    var_products = Product.objects.all()
+    return render(
+        request=request,
+        template_name='products.html',
+        context= {'products': var_products}
+        )
+
